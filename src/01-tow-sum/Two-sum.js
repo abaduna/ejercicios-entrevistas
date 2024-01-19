cd4d45f235dd94ae097d249d6a40d0f7efa598a1
abaduna~ <artuelrey@gmail.com>
2024-01-19 17:00:16 -0300

    terminedo

--- src/01-tow-sum/Two-sum.js
+++ src/01-tow-sum/Two-sum.js
@@ -2,7 +2,7 @@ function thuwSum(aray, target) {
   for (let index = 0; index < aray.length; index++) {
     const complmento = target - aray[index];
     const indiceDelComplemento = aray.findIndex((e) => e === complmento);
-    if (indiceDelComplemento !== 1) {
+    if (indiceDelComplemento !== -1) {
         return[index,indiceDelComplemento]
     }
   }
@@ -10,6 +10,6 @@ function thuwSum(aray, target) {
 }
 
 let aray = [1, 3, 7, 9, 2];
-let target = 4;
+let target = 11;
 console.log(thuwSum(aray, target))
 module.exports = {thuwSum}
